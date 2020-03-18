3ba7f8756b09f85982327892ea56bc35a3cd2b09
gobad820 <57797741+gobad820@users.noreply.github.com>
2020-03-19 08:35:19 +0900

    0319

--- max4.c
+++ max4.c
@@ -1,43 +1,46 @@
 #include <stdio.h>
 
-int max4(int a, int b, int c, int d){
+int max4(int a, int b, int c, int d)
+{
     int max = a;
-    if(b > max)
+    if (b > max)
         max = b;
-    if(c > max)
+    if (c > max)
         max = c;
-    if(d > max)
+    if (d > max)
         max = d;
-  return max;
+    return max;
 }
 
-int min3(int a, int b, int c){
+int min3(int a, int b, int c)
+{
     int min = a;
-    if( b < min)
+    if (b < min)
         min = b;
-    if(c < min)
+    if (c < min)
         min = c;
     return min;
 }
 
-int min4(int a, int b, int c,int d){
+int min4(int a, int b, int c, int d)
+{
     int min = a;
-    if( b < min)
+    if (b < min)
         min = b;
-    if(c < min)
+    if (c < min)
         min = c;
-    if(d < min)
+    if (d < min)
         min = d;
     return min;
 }
 
 int main(int argc, char const *argv[])
 {
-    int a,b,c,d;
-    scanf("%d %d %d %d",&a,&b,&c,&d);
-    printf("%d\n",max4(a,b,c,d));
-    printf("%d\n",min3(a,b,c));
-    printf("%d",min4(a,b,c,d));
-    
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("%d\n", max4(a, b, c, d));
+    printf("%d\n", min3(a, b, c));
+    printf("%d", min4(a, b, c, d));
+
     return 0;
 }
