09419e63d511432c60e6a6b7776289a949f85aa9
김상해 <gimsanghae@gimsanghaeui-MacBookPro-2.local>
2020-04-29 16:10:27 +0900

    RingBufferQueueFinal

---
+++ Unit4/Queue/RingBufferTest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define N 10
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int a[N];
+    int cnt = 0;
+    int retry;
+    puts("정수를 입력하세요");
+    do
+    {
+        printf("%d번째 정수를 입력하세요 : ", cnt + 1);
+        scanf("%d", &a[cnt++ % N]);
+        printf("계속할까요?(Yes..1 / No...0) : ");
+        scanf("%d", &retry);
+
+    } while (retry == 1);
+    i = cnt - N;
+    if (i < 0)
+        i = 0;
+    for (; i < cnt; i++)
+    {
+        printf("%2d번째 정수 = %d\n", i + 1, a[i % N]);
+    }
+    return 0;
+}
