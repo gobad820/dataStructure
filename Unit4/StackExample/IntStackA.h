f3b0ce259f07514b6442f8a011928158f5a24595
김상해 <gimsanghae@gimsanghaeui-MacBookPro-2.local>
2020-04-30 00:06:09 +0900

    Int

---
+++ Unit4/StackExample/IntStackA.h
@@ -0,0 +1,40 @@
+
+#ifndef __IntStack_H_
+#define __IntStack_H_
+
+typedef struct
+{
+    int max;
+    int ptr;
+    int *stk;
+} IntStack;
+
+int Initialize(IntStack *s, int max);
+
+int Push(IntStack *s, int x);
+
+int Pop(IntStack *s, int *x);
+
+int Peek(const IntStack *s, int *x);
+
+int Peek(const IntStack *s, int *x);
+
+void Clear(IntStack *s);
+
+void Clear(IntStack *s);
+
+int Size(const IntStack *s);
+
+int Capacity(const IntStack *s);
+
+int IsEmpty(const IntStack *s);
+
+int IsFull(const IntStack *s);
+
+int Search(const IntStack *s, int x);
+
+void Print(const IntStack *s);
+
+void Terminate(IntStack *s);
+
+#endif
