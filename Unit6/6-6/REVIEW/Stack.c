c68e2e43f04281ae996c027c820318eb9235f38e
김상해 <gimsanghae@gimsanghaeui-MacBookPro-2.local>
2020-05-23 13:31:06 +0900

    ver2

--- Unit6/6-6/REVIEW/Stack.c
+++ Unit6/6-6/REVIEW/Stack.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct
+{
 	int ptr;
 	int max;
-	int* stk;
-}Stack;
+	int *stk;
+} Stack;
 
-int Initialize(Stack* s, int size){
+int Initialize(Stack *s, int size)
+{
 	s->ptr = 0;
-	
-	if((s->stk = (int*)malloc(sizeof(int)*size)) == NULL){
+	if ((s->stk = (int *)malloc(sizeof(int) * size)) == NULL)
+	{
 		s->max = 0;
 		return -1;
 	}
@@ -18,80 +20,86 @@ int Initialize(Stack* s, int size){
 	return 0;
 }
 
-int Push(Stack* s, int number){
-	if(s->ptr == s->max)
+int Push(Stack *s, int number)
+{
+	if (s->ptr == s->max)
 		return -1;
 
 	s->stk[s->ptr] = number;
-	printf("%d is Pushed\n",number);
+	printf("%d is Pushed\n", number);
 	return 0;
 }
 
-int Pop(Stack* s, int* number){
-	if(s->ptr == 0 )
+int Pop(Stack *s, int *number)
+{
+	if (s->ptr == 0)
 		return -1;
 
 	*number = s->stk[--s->ptr];
-	printf("%d is Poped\n",*number);
+	printf("%d is Poped\n", *number);
 	return 0;
 }
 
-int isEmpty(Stack* s){
-	if(s->ptr == 0)
+int isEmpty(Stack *s)
+{
+	if (s->ptr == 0)
 		return 1;
 	return 0;
 }
 
-void Terminate(Stack* s){
-	if(s->stk != NULL)
+void Terminate(Stack *s)
+{
+	if (s->stk != NULL)
 		free(s->stk);
 	s->ptr = s->max = 0;
 }
 
-int main(void){
-	int index  = -1;
+int main(void)
+{
+	int index = -1;
 	puts("Size ? ");
 	int size;
-	scanf("%d",&size);
+	scanf("%d", &size);
 	Stack s;
-	if(Initialize(&s,size) == -1){
+	if (Initialize(&s, size) == -1)
+	{
 		puts("ERROR");
-		return 0;	
+		return 0;
 	}
 
-	while(1){
+	while (1)
+	{
 		printf(" 1 : Push / 2 : Pop / 3 : isEmpty / 4 : Terminate / 0 : Exit\n");
-		scanf("%d",&index);
+		scanf("%d", &index);
 		int number = 0;
-		switch(index){
-			case 1:
-				printf("Input the Number : ");
-				scanf("%d",&number);
-				if(Push(&s,number)== -1)
-					puts("The Stack Is Full");
-				break;
-			case 2:
-				if(Pop(&s,&number) == -1)
-					puts("The Stack is Empty");
-				printf("%d is Popped\n",number);
-				break;
-			case 3:
-				if(isEmpty(&s))
-					printf("This Stack Is Empty\n");
-				else
-					puts("This Stack is Not Empty");
-				break;
-			case 4:
-				puts("Terminate This Stack");
-				Terminate(&s);
-				index = 0;
-				break;
-			default :
-				puts("Wrong Index");
+		switch (index)
+		{
+		case 1:
+			printf("Input the Number : ");
+			scanf("%d", &number);
+			if (Push(&s, number) == -1)
+				puts("The Stack Is Full");
+			break;
+		case 2:
+			if (Pop(&s, &number) == -1)
+				puts("The Stack is Empty");
+			printf("%d is Popped\n", number);
+			break;
+		case 3:
+			if (isEmpty(&s))
+				printf("This Stack Is Empty\n");
+			else
+				puts("This Stack is Not Empty");
+			break;
+		case 4:
+			puts("Terminate This Stack");
+			Terminate(&s);
+			index = 0;
+			break;
+		default:
+			puts("Wrong Index");
 		}
-	if(index == 0)
-		break;
+		if (index == 0)
+			break;
 	}
-	
 }
-		
